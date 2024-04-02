aa803985ac48cab3acc350e14b52a8bcb0c22737
tMoulton99 <tlm@students.uwf.edu>
2024-04-02 10:45:19 -0500

    timLab1

--- src/app/controllers/app.controller.ts
+++ src/app/controllers/app.controller.ts
@@ -15,9 +15,37 @@ export class AppController {
     this.router.get("/", (req: Request, res: Response) => {
       try {
         // Render the "home" template as HTML
-        res.render("home");
+        res.render("home", {
+          user: req.session.user;
+        });
       } catch (err) {
         this.log.error(err);
+      }
+
+
+    });
+
+    
+    this.router.get("/login", (req: Request, res: Response) => {
+      res.render("login");
+    });
+
+    this.router.post("/login", (req: any, res: Response) => {
+      req.session.user = req.body.username;
+      res.redirect("/");
+    });
+
+    this.router.get("/logout", (req: any, res: Response) => {
+      req.session.destroy(() => {
+        res.redirect("/");
+      });
+    });
+
+    this.router.use((req: any, res: Response, next) => {
+      if (req.session.user) { 
+        next();
+      } else {
+        res.redirect("/login");
       }
     });
     
